c990a2f9982e2fe0918953eb7500d8ac0954c6a2
Andrii Bohach <bogach_andrij@vu.cdu.edu.ua>
2020-12-21 13:51:31 +0200

    Створено найпростіший консольний додаток

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace МТЗКІТ_лаба__6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            git config user.name "Andrij Bohach"
+        }
+    }
+}
